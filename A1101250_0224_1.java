94f71b2408fa73a041dafdeaecb896c259712e5e
kevin <125640544+a1101250@users.noreply.github.com>
2023-02-28 12:53:16 +0800

    Add files via upload

---
+++ A1101250_0224_1.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+public class A1101250_0224_1 {
+    public static void main(String[] argv) {
+      int number;
+      System.out.println("請輸入任一整數");
+      Scanner sc = new Scanner(System.in);
+      number = sc.nextInt();
+    if (number%2==0) {
+        System.out.println("此整數為偶數");
+    } else {
+        System.out.println("此整數為奇數");
+    } 
+
+
+
+
+
+
+
+    }
+
+
+
+
+
+
+}
