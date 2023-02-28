8497c313743c3cb2595b2096cf0e1e5d94dca582
kevin <125640544+a1101250@users.noreply.github.com>
2023-02-28 12:53:50 +0800

    Add files via upload

---
+++ A1101250_0224_2.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class A1101250_0224_2 {
+    public static void main(String[] argv) {
+        float x;
+        float y;
+        System.out.println("請輸入攝氏溫度");
+        Scanner sc = new Scanner(System.in);
+        x= sc.nextInt();
+        y=9*x/5+32;
+        System.out.println("華氏溫度為"+ y + "度");
+
+
+
+    }
+}
