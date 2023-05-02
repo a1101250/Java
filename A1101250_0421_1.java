7cc4556d57149c33bfa6e6003d6171a59fae0b5c
kevin <125640544+a1101250@users.noreply.github.com>
2023-05-02 16:29:21 +0800

    Add files via upload

---
+++ A1101250_0421_1.java
@@ -0,0 +1,17 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class A1101250_0421_1 {
+    public static void main(String[] argv)  throws Exception {
+        BufferedReader br =
+        new BufferedReader(new InputStreamReader(System.in));
+
+        String str;
+        System.out.println("請輸入你的e-mail(只有小寫字母)");
+        str = br.readLine();
+        System.out.println("你的e-mail: " + str);
+    }
+    
+}
+
